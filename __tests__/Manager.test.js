5a34cdabe9fbf744b2eb0f265ec3473b04f80a0c
Nadine <nadine.mohsen@hotmail.com>
2022-03-02 16:51:58 +0000

    Intern test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -5,12 +5,12 @@ const Manager = require('../lib/Manager');
 
 //creating manager object
 test('creates a Manager object',() =>{
-    const manager = new Manager('Nadine',10,'nadine.mohsen@hotmail.com',201);
+    const manager = new Manager('Nadine',10,'nadine@example.com',201);
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // gets role from getRole()
 test('gets role of employee', () => {
-    const manager = new Manager('Nadine', 10, 'nadine.mohsen@hotmail.com');
+    const manager = new Manager('Nadine', 10, 'nadine@example.com');
     expect(manager.getRole()).toEqual("Manager");
 }); 

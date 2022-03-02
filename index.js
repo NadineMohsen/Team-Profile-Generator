05e1eb35de1305c0af84a4a9e3f4fb8b8c44c9c5
Nadine <nadine.mohsen@hotmail.com>
2022-03-02 15:01:32 +0000

    add Employee (Engineer or intern)

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
 
 const fs = require('fs');
-const generateTeam = require('./src/generateHTML');
 
 // link to page creation
 const generateHTML = require('./src/generateHTML');
@@ -72,11 +71,120 @@ const addManager = () => {
         }
     ]) 
     .then(managerInput => {
+        //creates new manager
         const {name,id,email,officeNumber} = managerInput;
-        const manager = new Manager (name,id,email.officeNumber)
+        const manager = new Manager (name,id,email,officeNumber)
 
         teamArray.push(manager)
         console.log(manager)
 
+    })
+}
+//Prompts for employee
+const addEmployee =() => {
+    console.log(`Adding employees to the team! `)
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Please choose your employee's role",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What's the name of the employee?", 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter an employee's name!");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter the employee's ID.",
+            validate: nameInput => {
+                if  (isNaN(nameInput)) {
+                    console.log ("Please enter the employee's ID!")
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the employee's email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!')
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Please enter the employee's github username.",
+            when: (input) => input.role === "Engineer",
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ("Please enter the employee's github username!")
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Please enter the intern's school",
+            when: (input) => input.role === "Intern",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter the intern's school!")
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to add more team members?',
+            default: false
+        }
+    ])
+    .then(employeData => {
+        //data for employees
+        let { name, id, email, role, github, school, confirmAddEmployee } = employeeData; 
+        let employee; 
+        // Case Engineer
+        if (role === "Engineer") {
+            employee = new Engineer (name, id, email, github);
+
+            console.log(employee);
+        // Case Intern
+        } else if (role === "Intern") {
+            employee = new Intern (name, id, email, school);
+
+            console.log(employee);
+        }
+        //adds employee to the array
+        teamArray.push(employee); 
+
+        if (confirmAddEmployee) {
+            return addEmployee(teamArray); 
+        } else {
+            return teamArray;
+        }
     })
 }
